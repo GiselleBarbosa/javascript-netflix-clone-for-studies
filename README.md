c5246b93b79a97fd2e17bf25a348d2825d805b75
GiselleBarbosa <g88barbosa@gmail.com>
2022-12-18 20:51:13 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Netflix clone for studies
+
+Clone da Página inicial da Netflix com consumo de API.
+
+Linguagens utilizadas:
+-html /css
+-Javascript
---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "e4e9b82f52f8c5dcde267f96d1b2e280";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
